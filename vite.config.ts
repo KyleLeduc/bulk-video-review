119ebbc43915595c5deec2ff3adb1c4e55ac1738
Kyle Leduc <72325360+KyleLeduc@users.noreply.github.com>
2024-06-23 13:19:05 -0600

    fix: update base public path to match repo name

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/bvr/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/bulk-video-review/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
